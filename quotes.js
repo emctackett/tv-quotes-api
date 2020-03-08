b3bb956fef5b91d71fd540dc2a6997d3b2b551df
Elizabeth Tackett <emctackett@gmail.com>
2020-03-08 14:16:03 -0500

    initial commit

---
+++ quotes.js
@@ -0,0 +1,701 @@
+module.exports = [
+  {
+    id: 1,
+    show: "schitt's creek",
+    quote: "That's a real quick no.",
+    character: "david rose"
+  },
+  {
+    id: 2,
+    show: "schitt's creek",
+    quote: "I plan on popping a pill, crying a bit, and falling asleep early.",
+    character: "david rose"
+  },
+  {
+    id: 3,
+    show: "schitt's creek",
+    quote: "I'm trying very hard not to connect to people right now.",
+    character: "david rose"
+  },
+  {
+    id: 4,
+    show: "schitt's creek",
+    quote: "Well, she sort of fades into the background after a " +
+      " while.  You know, like a smoke alarm.",
+    character: "david rose"
+  },
+  {
+    id: 5,
+    show: "schitt's creek",
+    quote: "I have asked you thrice now for a towel.",
+    character: "david rose"
+  },
+  {
+    id: 6,
+    show: "schitt's creek",
+    quote: "Oh my God. No.",
+    character: "david rose"
+  },
+  {
+    id: 7,
+    show: "schitt's creek",
+    quote: "I could not be more at one with nature, I do coachella every year.",
+    character: "david rose"
+  },
+  {
+    id: 8,
+    show: "schitt's creek",
+    quote: "I like the wine and not the label. Does that make sense?",
+    character: "david rose"
+  },
+  {
+    id: 9,
+    show: "schitt's creek",
+    quote: "It's my turn to take a selfish!",
+    character: "david rose"
+  },
+  {
+    id: 10,
+    show: "schitt's creek",
+    quote: "Very uninterested in that opinion.",
+    character: "david rose"
+  },
+  {
+    id: 11,
+    show: "schitt's creek",
+    quote: "Unlike Beyonce, I don't excel as a solo artist.",
+    character: "david rose"
+  },
+  {
+    id: 12,
+    show: "schitt's creek",
+    quote: "So I'm just going to play the supportive partner and watch him fail.",
+    character: "david rose"
+  },
+  {
+    id: 13,
+    show: "schitt's creek",
+    quote: "I have never heard someone say so many wrong things, " +
+      "one after the other, consecutively, in a row.",
+    character: "david rose"
+  },
+  {
+    id: 14,
+    show: "schitt's creek",
+    quote: "The Internet is a breeding ground for freaks.",
+    character: "david rose"
+  },
+  {
+    id: 15,
+    show: "schitt's creek",
+    quote: "I'm gonna need a stiff drink to get through this.",
+    character: "david rose"
+  },
+  {
+    id: 16,
+    show: "schitt's creek",
+    quote: "Don't be a little b!",
+    character: "david rose"
+  },
+  {
+    id: 17,
+    show: "schitt's creek",
+    quote: "I'm feeling this, like, deep aching sense of dread.",
+    character: "david rose"
+  },
+  {
+    id: 18,
+    show: "schitt's creek",
+    quote: "Eat glass!",
+    character: "david rose"
+  },
+  {
+    id: 19,
+    show: "schitt's creek",
+    quote: "Every inch of that sentence made me sick.",
+    character: "david rose"
+  },
+  {
+    id: 20,
+    show: "schitt's creek",
+    quote: "I am 87% behind you.",
+    character: "david rose"
+  },
+  {
+    id: 21,
+    show: "schitt's creek",
+    quote: "Fall off a bridge, please.",
+    character: "david rose"
+  },
+  {
+    id: 22,
+    show: "schitt's creek",
+    quote: "What kind of barnyard were you raised in?",
+    character: "david rose"
+  },
+  {
+    id: 23,
+    show: "schitt's creek",
+    quote: "Mama Oprah would be very proud of us.",
+    character: "david rose"
+  },
+  {
+    id: 24,
+    show: "schitt's creek",
+    quote: "Would we not all agree that words are weapons?",
+    character: "david rose"
+  {
+    id: 25,
+    show: "schitt's creek",
+    quote: "I will not feel shame about the mall pretzels.",
+    character: "david rose"
+  },
+  {
+    id: 26,
+    show: "schitt's creek",
+    quote: "I was perfectly fine not trusting people. Not trusting " +
+      "people is what I'm used to. It is my comfort zone.",
+    character: "david rose"
+  },
+  {
+    id: 27,
+    show: "schitt's creek",
+    quote: "If you are any kind of friend you will let me cling to those lies.",
+    character: "david rose"
+  },
+  {
+    id: 28,
+    show: "schitt's creek",
+    quote: "Thank you so much for this. It was entirely unhelpful.",
+    character: "david rose"
+  {
+    id: 29,
+    show: "schitt's creek",
+    quote: "You get murdered first!",
+    character: "david rose"
+  },
+  {
+    id: 30,
+    show: "schitt's creek",
+    quote: "I'm gonna pass. I'm not really in the mood to be a victim " +
+    "of a hate crime tonight.",
+    character: "david rose"
+  },
+  {
+    id: 31,
+    show: "schitt's creek",
+    quote: "I saw a lot of my body, and I didn't like it.",
+    character: "david rose"
+  },
+  {
+    id: 32,
+    show: "schitt's creek",
+    quote: "Shame on you for attempting that position at 8 o'clock in " +
+      "the morning!",
+    character: "david rose"
+  },
+  {
+    id: 33,
+    show: "schitt's creek",
+    quote: "You just watch a season of Girls and do the opposite of what " +
+      "they do.  It's easy.",
+    character: "david rose"
+  },
+  {
+    id: 34,
+    show: "schitt's creek",
+    quote: "Well, this might be the closest we're going to get to Meryl. " +
+      "You know, if she got fired from her job at the magazine " +
+      "and started walking the streets.",
+    character: "david rose"
+  },
+  {
+    id: 35,
+    show: "schitt's creek",
+    quote: "Is there like, a Texas Chainsaw movie being filmed out there " +
+      "that I'm not aware of?",
+    character: "david rose"
+  },
+  {
+    id: 36,
+    show: "schitt's creek",
+    quote: "Ugh!  It smells like pennies and burps.",
+    character: "david rose"
+  },
+  {
+    id: 37,
+    show: "new girl",
+    quote: "I don't celebrate Christmas.  Or, as I like to call it, " +
+      "'White Anglo Saxon Winter Privilege Night'.",
+    character: "schmidt"
+  },
+  {
+    id: 38,
+    show: "new girl",
+    quote: "I had figure skating lessons until I was thirteen. " +
+      "Then my mom sobered up and realized I was a boy.",
+    character: "schmidt"
+  },
+  {
+    id: 39,
+    show: "new girl",
+    quote: "Old people freak me out. With their hands and their legs. " +
+      "They're like the people version of pleated pants.",
+    character: "schmidt"
+  },
+  {
+    id: 40,
+    show: "new girl",
+    quote: "Know this: your caveman ideas about manhood are so over. Manhood " +
+      "today is about exfoliation, cheese courses, emotional honesty, and Paxil.",
+    character: "schmidt"
+  },
+  {
+    id: 41,
+    show: "new girl",
+    quote: "The economy stinks, bees are dying, and movies are pretty much " +
+      "all sequels now.",
+    character: "schmidt"
+  },
+  {
+    id: 42,
+    show: "new girl",
+    quote: "Damn it! I can't find my driving moccasins anywhere!",
+    character: "schmidt"
+  },
+  {
+    id: 43,
+    show: "new girl",
+    quote: "Youths!",
+    character: "schmidt"
+  },
+  {
+    id: 44,
+    show: "new girl",
+    quote: "Are you cooking a frittata in a sauce pan?  What is this-- prison?",
+    character: "schmidt"
+  },
+  {
+    id: 45,
+    show: "new girl",
+    quote: "If you need me, I'll be in my room, listening to mainstream hip-hop.",
+    character: "schmidt"
+  },
+  {
+    id: 46,
+    show: "new girl",
+    quote: "Can someone please get my towel? It's in my room next to my Irish " +
+      "walking cape!",
+    character: "schmidt"
+  },
+  {
+    id: 47,
+    show: "new girl",
+    quote: "I cried the other day listening to a techno song. My tweets " +
+      "have been extremely literal.",
+    character: "schmidt"
+  },
+  {
+    id: 48,
+    show: "new girl",
+    quote: "Pine has no place in this house.  It's the wood of poor people " +
+      "and outhouses.",
+    character: "schmidt"
+  },
+  {
+    id: 49,
+    show: "new girl",
+    quote: "I can do anything I put my mind to.  I once figured out Alyssa " +
+      "Milano's phone number just by randomly choosing numbers.",
+    character: "schmidt"
+  },
+  {
+    id: 50,
+    show: "new girl",
+    quote: "Have you seen my sharkskin laptop sleeve?",
+    character: "schmidt"
+  },
+  {
+    id: 51,
+    show: "new girl",
+    quote: "I can't talk right now. I'm writing a strongly worded " +
+      "e-mail to my florist.",
+    character: "schmidt"
+  },
+  {
+    id: 52,
+    show: "new girl",
+    quote: "Bathtubs are medieval filth cauldrons.",
+    character: "schmidt"
+  },
+  {
+    id: 53,
+    show: "new girl",
+    quote: "Befriending Kanye is the most efficient way for me to jump social " +
+      "strata. Now all I have to do is meet him, and then dazzle him to the " +
+      "point that we're each other's last call before bed.",
+    character: "schmdit"
+  },
+  {
+    id: 54,
+    show: "new girl",
+    quote: "It's like you're ripping the side block out of my mental jenga.",
+    character: "schmidt"
+  },
+  {
+    id: 55,
+    show: "new girl",
+    quote: "I'm not being overdramatic when I say I would rather sit naked " +
+      "on a hot grill than wear something off the rack.",
+    character: "schmidt"
+  },
+  {
+    id: 56,
+    show: "new girl",
+    quote: "Now as my best friend I expect you to be willing to sacrifice " +
+      "everything for me at a moment's notice including, of course, " +
+      "unfortunately for you, your dignity",
+    character: "schmidt"
+  },
+  {
+    id: 57,
+    show: "new girl",
+    quote: "Where do you even buy sheets like this? They have the thread " +
+      "count of a paper towel.",
+    character: "schmidt"
+  },
+  {
+    id: 58,
+    show: "new girl",
+    quote: "Well, you have found my flabbergast button, and guess what? " +
+      "You've pressed it.",
+    character: "schmidt"
+  },
+  {
+    id: 59,
+    show: "new girl",
+    quote: "It makes me angry and scared all at the same time. Just like when " +
+      "I hear the phrase, 'Academy Award winner Anna Paquin'.",
+    character: "schmidt"
+  },
+  {
+    id: 60,
+    show: "new girl",
+    quote: "Walk with dignity, you giant toddler!",
+    character: "schmidt"
+  },
+  {
+    id: 61,
+    show: "new girl",
+    quote: "You were denied a cell phone because you have the credit score " +
+      "of a homeless ghost!",
+    character: "schmidt"
+  },
+  {
+    id: 62,
+    show: "new girl",
+    quote: "Now, if you'll excuse me, I have to go water my succulents.",
+    character: "schmidt"
+  },
+  {
+    id: 63,
+    show: "new girl",
+    quote: "You're listening to the radio and writing with a pen. " +
+      "What decade are we in?",
+    character: "schmidt"
+  },
+  {
+    id: 64,
+    show: "new girl",
+    quote: "Do you just walk aound all day thinking about other people's " +
+      "feelings? How do you get anything done?",
+    character: "schmidt"
+  },
+  {
+    id: 65,
+    show: "new girl",
+    quote: "Well, I suppose we could just get out and ask any of these " +
+      "street youths.",
+    character: "schmidt"
+  },
+  {
+    id: 66,
+    show: "new girl",
+    quote: "Please take that hat off. You look like a homeless pencil.",
+    character: "schmidt"
+  },
+  {
+    id: 67,
+    show: "new girl",
+    quote: "Oh good God! Is that a common barber's comb?",
+    character: "schmidt"
+  },
+  {
+    id: 68,
+    show: "new girl",
+    quote: "Use the tongs, honey. This isn't Appalachia.",
+    character: "schmidt"
+  },
+  {
+    id: 69,
+    show: "new girl",
+    quote: "You like my thumb ring? So nectar.",
+    character: "schmidt"
+  },
+  {
+    id: 70,
+    show: "new girl",
+    quote: "Much like Jay-Z, I too have 99 problems. But unlike Jay-Z, " +
+      "I have zero caveats.",
+    character: "schmidt"
+  },
+  {
+    id: 71,
+    show: "new girl",
+    quote: "I'm gonna have to run all the way home, and I have my " +
+      "slippiest loafers on!",
+    character: "schmidt"
+  },
+  {
+    id: 72,
+    show: "new girl",
+    quote: "Cream cheese in bulk!?",
+    character: "schmidt"
+  },
+  {
+    id: 73,
+    show: "parks and recreation",
+    quote: "Oh, am I wearing an ascot? I didn't notice.",
+    character: "tom haverford"
+  },
+  {
+    id: 74,
+    show: "parks and recreation",
+    quote: "Sometimes you gotta work a little, so you can ball a lot.",
+    character: "tom haverford"
+  },
+  {
+    id: 75,
+    show: "parks and recreation",
+    quote: "If your job was remotely interesting, there would be a show " +
+      "on A&E about it.",
+    character: "tom haverford"
+  },
+  {
+    id: 76,
+    show: "parks and recreation",
+    quote: "I have never taken the high road. But I tell other people to " +
+      "'cause then there's more room for me on the low road.",
+    character: "tom haverford"
+  },
+  {
+    id: 77,
+    show: "parks and recreation",
+    quote: "The four sweetest words in the English language: you wore me down.",
+    character: "tom haverford"
+  },
+  {
+    id: 78,
+    show: "parks and recreation",
+    quote: "You can't say your favorite kind of cake is birthday cake. That's " +
+      "like saying your favorite kind of cereal is breakfast cereal.",
+    character: "tom haverford"
+  },
+  {
+    id: 79,
+    show: "parks and recreation",
+    quote: "Zerts are what I call desserts. Tray-trays are entrees.",
+    character: "tom haverford"
+  },
+  {
+    id: 80,
+    show: "parks and recreation",
+    quote: "People in this town are just getting into Nirvana. I don't " +
+      "have the heart to tell them what's gonna happen to Kurt Cobain in 1994.",
+    character: "tom haverford"
+  },
+  {
+    id: 81,
+    show: "parks and recreation",
+    quote: "I can't keep referring to basketball players as Khloe " +
+      "Kardashian's husband and his friends.",
+    character: "tom haverford"
+  },
+  {
+    id: 82,
+    show: "parks and recreation",
+    quote: "Most people would say 'the deets', but I say 'the tails'. " +
+      "Just another example of innovation.",
+    character: "tom haverford"
+  },
+  {
+    id: 83,
+    show: "parks and recreation",
+    quote: "This morning I saw a YouTube video with a puppy riding a " +
+      "motorcycle. So my bar for stunning is pretty high.",
+    character: "tom haverford"
+  },
+  {
+    id: 84,
+    show: "parks and recreation",
+    quote: "If all goes well, this might be one of the last times I get to " +
+      "speak to you.",
+    character: "tom haverford"
+  },
+  {
+    id: 85,
+    show: "parks and recreation",
+    quote: "I got you a going away present. I'm finally deleting you from my " +
+      "phone",
+    character: "tom haverford"
+  },
+  {
+    id: 86,
+    show: "parks and recreation",
+    quote: "At the risk of bragging, one of the things I'm best at is riding " +
+      "coattails. Behind every successful man is me, taking partial credit.",
+    character: "tom haverford"
+  },
+  {
+    id: 87,
+    show: "parks and recreation",
+    quote: "All sushi is made by fish, previously owned by celebrities.",
+    character: "tom haverford"
+  },
+  {
+    id: 88,
+    show: "parks and recreation",
+    quote: "Can you put some more tiny marshmellows in my hot choccy?",
+    character: "tom haverford"
+  },
+  {
+    id: 89,
+    show: "parks and recreation",
+    quote: "One time my refrigerator stopped working and I had no idea " +
+      "what to do. I just moved.",
+    character: "tom haverford"
+  },
+  {
+    id: 90,
+    show: "parks and recreation",
+    quote: "May I saw for the record that is a dope pocket square?",
+    character: "tom haverford"
+  },
+  {
+    id: 91,
+    show: "parks and recreation",
+    quote: "This is like listening to a Ted Talk by the color beige!",
+    character: "tom haverford"
+  },
+  {
+    id: 92,
+    show: "parks and recreation",
+    quote: "Am I team Edward? Yes. Do I share his concerns about turning " +
+      "Bella, though? Absolutely not.",
+    character: "tom haverford"
+  },
+  {
+    id: 93,
+    show: "parks and recreation",
+    quote: "Despite what my pocket square says, I'm not a billionaire.",
+    character: "tom haverford"
+  },
+  {
+    id: 94,
+    show: "parks and recreation",
+    quote: "Love fades  away, but things... things are forever.",
+    character: "tom haverford"
+  },
+  {
+    id: 95,
+    show: "parks and recreation",
+    quote: "I strapped an MP3 player to one of those floor-cleaning robots. " +
+      "Call him DJ Roomba.",
+    character: "tom haverford"
+  },
+  {
+    id: 96,
+    show: "parks and recreation",
+    quote: "No, I don't text her 'It was nice meeting you.' I wait eight weeks " +
+      "and I text her 'What's crackin'?''",
+    character: "tom haverford"
+  },
+  {
+    id: 97,
+    show: "parks and recreation",
+    quote: "Every song I download has to pass a series of rigorous tests to " +
+      "answer one simple question: is it a banger?",
+    character: "tom haverford"
+  },
+  {
+    id: 98,
+    show: "parks and recreation",
+    quote: "Oh, what's this inside my shoe? Red carpet insole. Everywhere " +
+      "I go, I'm walking on red carpet.",
+    character: "tom haverford"
+  },
+  {
+    id: 99,
+    show: "parks and recreation",
+    quote: "Treat Yo'self",
+    character: "tom haverford"
+  },
+  {
+    id: 100,
+    show: "parks and recreation",
+    quote: "Watch the master work it, I am the Yoda of networking.",
+    character: "tom haverford"
+  },
+  {
+    id: 101,
+    show: "parks and recreation",
+    quote: "Man, I'm hungry and my legs are tired. It feels like I just " +
+      "exercised.",
+    character: "tom haverford"
+  },
+  {
+    id: 102,
+    show: "parks and recreation",
+    quote: "It's dirty and I'm wearing my summer linens!",
+    character: "tom haverford"
+  },
+  {
+    id: 103,
+    show: "parks and recreation",
+    quote: "We specialize in making stacks on stacks on stacks.",
+    character: "tom haverford"
+  },
+  {
+    id: 104,
+    show: "parks and recreation",
+    quote: "",
+    character: "tom haverford"
+  },
+  {
+    id: 105,
+    show: "parks and recreation",
+    quote: "I hate doing work, but I love being flattered. " +
+      "So maybe I'll give it another try.",
+    character: "tom haverford"
+  },
+  {
+    id: 106,
+    show: "parks and recreation",
+    quote: "How 'bout I break you off some of that Ja Rule?",
+    character: "tom haverford"
+  },
+  {
+    id: 107,
+    show: "parks and recreation",
+    quote: "A straightforward deal! Why didn't you tell me? I don't have " +
+      "my straightforward fedora on me! We gotta stop at my storage unit!",
+    character: "tom haverford"
+  },
+  {
+    id: 108,
+    show: "parks and recreation",
+    quote: "That sweat suit is not for sweating. If you take that crushed " +
+      "velvet on more than a brisk walk, it'll fall apart.",
+    character: "tom haverford"
+  }
+];
